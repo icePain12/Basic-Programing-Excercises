321bb7914ca829878a926e6020c030ebb8adb629
Luis David Bocanegra <54543119+icePain12@users.noreply.github.com>
2019-11-08 17:50:24 -0600

    Update hello_world.cs

--- hello world/C#/hello_world.cs
+++ hello world/C#/hello_world.cs
@@ -1,3 +1,4 @@
+/* In This program we will show the hello world but in a method in order to get used to how objet oriented programming works */
 using System;
 namespace Hello
 {
@@ -5,12 +6,16 @@ namespace Hello
     {
         static void Main(String[] args)
         {
+            // This method will put in blank your console when the program starts.
             Console.Clear();
+            // This Line is Optional It will put your console in Green.
              Console.ForegroundColor = ConsoleColor.Green;
-             Saludar();
+            // Whe call the method "Salute" in order to Run what it does. That is to print: "Hello World" in your console.
+             Salute();
              Console.ReadKey();   
         }
-        static void Saludar()
+        // we declare a method call Salute to run the hello world program.
+        static void Salute()
         {
               Console.Write("Hello World...");
         }
